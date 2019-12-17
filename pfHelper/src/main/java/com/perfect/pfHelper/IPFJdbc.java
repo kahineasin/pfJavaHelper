8729f715e5e9f629db6750a87881f6b05cf62832
Administrator <Administrator@jmenv.tbsite.net>
2019-12-17 10:34:05 +0800

    pfHelper to github

---
+++ pfHelper/src/main/java/com/perfect/pfHelper/IPFJdbc.java
@@ -0,0 +1,18 @@
+package com.perfect.pfHelper;
+
+public interface IPFJdbc {
+
+     String getUrl();
+
+     void setUrl(String url);
+     String getDriverClassName() ;
+
+     void setDriverClassName(String driverClassName) ;
+     String getUsername() ;
+
+     void setUsername(String username);
+
+     String getPassword();
+
+     void setPassword(String password);
+}
